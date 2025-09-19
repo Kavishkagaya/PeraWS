00a333f7300af3ca18481b888dd7903007ff0407
Kavishka <kavishkarambukwella@gmail.com>
2025-09-19 16:13:38 +0530

    initialize is done

--- functions/dispatcher/services/podManager.js
+++ functions/dispatcher/services/podManager.js
@@ -1,50 +1,15 @@
-const client = require('@kubernetes/client-node')
+const {createPod, deletePod} = require('./podUtilities')
 
-const kc = new client.KubeConfig()
-kc.loadFromDefault()
+const podPool = {};
 
-const k8sApi = kc.makeApiClient(client.CoreV1Api)
-const excec = new client.Exec()
+const pods = JSON.parse(require('./config.json')).pods;
 
-const createPod = async (podManifest, namespace) => {
-    try {
-        const response = await k8sApi.createNamespacedPod(namespace, podManifest)
-        return response.body
-    } catch (error) {
-        console.error('Error creating pod:', error)
-        throw error
-    }
-}
-
-const deletePod = async (podName, namespace) => {
-    try {
-        await k8sApi.deleteNamespacedPod(podName, namespace)
-        console.log(`Pod ${podName} deleted successfully`)
-    }
-    catch (error) {
-        console.error('Error deleting pod:', error)
-        throw error
-    }
-}
-
-const execInPod = async (podName, namespace, command) => {
-    return new Promise((resolve, reject) => {
-        const stdout = []
-        const stderr = []
-
-        excec.exec(namespace, podName, 'container-name', command,
-            process.stdout, process.stderr, null, false /* tty */, (status) => {
-                if (status.status === 'Success') {
-                    resolve(stdout.join(''))
-                } else {
-                    reject(new Error(stderr.join('')))
-                }
-            })
-    })
-}
-
-module.exports = {
-    createPod,
-    deletePod,
-    execInPod
-}
+const initPodPool = () => {
+    Object.keys(pods).forEach(podName => {
+        const podConfig = pods[podName];
+        podPool[podName] = [];
+        for (let i = 0; i < podConfig.replicas; i++) {
+            podPool[podName].push(createPod(podConfig.manifest, podName));
+        }
+    });
+};
---
+++ functions/dispatcher/services/podUtilities.js
@@ -0,0 +1,51 @@
+const client = require('@kubernetes/client-node')
+
+const kc = new client.KubeConfig()
+kc.loadFromDefault()
+
+const k8sApi = kc.makeApiClient(client.CoreV1Api)
+const excec = new client.Exec()
+
+const createPod = async (namespace, podManifest) => {
+    try {
+        const response = await k8sApi.createNamespacedPod(namespace, podManifest)
+        console.log('Pod created successfully:', response.body)
+        return response.body
+    } catch (error) {
+        console.error('Error creating pod:', error)
+        throw error
+    }
+}
+
+const deletePod = async (podName, namespace) => {
+    try {
+        await k8sApi.deleteNamespacedPod(podName, namespace)
+        console.log(`Pod ${podName} deleted successfully`)
+    }
+    catch (error) {
+        console.error('Error deleting pod:', error)
+        throw error
+    }
+}
+
+const execInPod = async (podName, namespace, command) => {
+    return new Promise((resolve, reject) => {
+        const stdout = []
+        const stderr = []
+
+        excec.exec(namespace, podName, 'container-name', command,
+            process.stdout, process.stderr, null, false /* tty */, (status) => {
+                if (status.status === 'Success') {
+                    resolve(stdout.join(''))
+                } else {
+                    reject(new Error(stderr.join('')))
+                }
+            })
+    })
+}
+
+module.exports = {
+    createPod,
+    deletePod,
+    execInPod
+}
